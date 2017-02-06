95aefc48872df808ebd8d6d6ce6aa585ff04dd69
Evan Gatchell <greygatch@gmail.com>
2017-02-06 11:50:25 -0600

    polished promise example.

--- callbacks.js
+++ callbacks.js
@@ -4,5 +4,15 @@ request('http://swapi.co/api/people/1', (err, response, body) => {
   if(err){
     console.log('ERROR:', err);
   }
-  console.log(body);
+  console.log('first response:', body);
+  console.log('**************');
+
+  request('http://swapi.co/api/planets/3/', (err, response, body) => {
+    if(err){
+      console.log('ERROR', err);
+    }
+    console.log('second response:', body);
+    console.log('**************');
+  });
+
 })
--- promises.js
+++ promises.js
@@ -1,9 +1,26 @@
 const request = require('request-promise');
 
-request('http://swapi.co/api/people/1')
+// request('http://swapi.co/api/people/1')
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log('error:', err)
+//   });
+
+/*****************/
+
+const getPerson = request('http://swapi.co/api/people/1');
+const getPlanet = request('http://swapi.co/api/planets/3');
+
+Promise.all([getPerson, getPlanet])
   .then(response => {
-    console.log(response);
+    console.log('first response:', response[0]);
+    console.log('***************');
+    console.log('second response:', response[1]);
   })
   .catch(err => {
-    console.log('error:', err)
-  })
+    console.log(err);
+  });
+
+/*****************/
