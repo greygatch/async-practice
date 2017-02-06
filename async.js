9f4132218996be084408f93b1dbd0ab358a1e9d6
Evan Gatchell <greygatch@gmail.com>
2017-02-06 12:00:02 -0600

    installed babel-cli for async/await stuff

--- async.js
+++ async.js
@@ -1,9 +1,16 @@
-const request = require('request');
+const request = require('request-promise');
 
 async function getStuff(){
   try{
+    const person = await request('http://swapi.co/api/people/1');
+    const planet = await request('http://swapi.co/api/planets/3');
 
+    console.log('first response:', person);
+    console.log('***************');
+    console.log('second response:', planet);
   }catch(err){
     console.log('error:', err)
   }
 }
+
+getStuff();
